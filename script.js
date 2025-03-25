4f0032a38f207803f75113d0b083e09c330f9e66
Izan Mendoza <izanmg2706@gmail.com>
2025-03-25 17:29:40 +0100

    Creacion de funcion de scroll

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function copyEmail() {
         }, 2000);
     });
 }
+
+// Obtener todos los enlaces de la navegación
+const enlaces = document.querySelectorAll('.ref');
+
+// Agregar un event listener a cada enlace para evitar el comportamiento por defecto
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function(event) {
+        // Prevenir el comportamiento por defecto del enlace (cargar la página)
+        event.preventDefault();
+
+        // Obtener el id de la sección correspondiente
+        const seccionId = this.getAttribute('href');
+
+        // Hacer scroll hacia la sección de manera suave
+        document.querySelector(seccionId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
